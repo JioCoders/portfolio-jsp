93e97a7457f5a364f6c9dc443e64d5b08938dc0c
harry <hk.mca08@gmail.com>
2025-06-23 08:38:08 +0530

    add/GET-POST_api

--- src/main/java/com/jiocoders/portfolio/PortfolioController.java
+++ src/main/java/com/jiocoders/portfolio/PortfolioController.java
@@ -1,13 +1,30 @@
 package com.jiocoders.portfolio;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Map;
 
-@Controller
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PortfolioController {
     
+    // Serve JSP page
     @GetMapping({"/", "/portfolio"})
     public String home() {
         return "index"; // maps to /WEB-INF/jsp/index.jsp
+    }
+
+    // Serve JSON response
+    // GET API: /api/hello
+    @GetMapping("/api/hello")
+    public Map<String, String> helloApi() {
+        return Map.of("message", "Hi, I'm Jiocoders team");
+    }
+
+    // POST API: /api/hello
+    @PostMapping("/api/hello")
+    public Map<String, String> helloPost() {
+        return Map.of("message", "Hi, I'm Jiocoders team");
     }
 }
