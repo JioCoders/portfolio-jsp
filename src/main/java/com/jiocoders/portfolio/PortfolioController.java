2b06fe1f793a1f9a5664a4f2f35fbb6d9eea457c
harry <hk.mca08@gmail.com>
2025-06-23 21:25:09 +0530

    update/seperate_controller

--- src/main/java/com/jiocoders/portfolio/PortfolioController.java
+++ src/main/java/com/jiocoders/portfolio/PortfolioController.java
@@ -1,30 +1,14 @@
 package com.jiocoders.portfolio;
 
-import java.util.Map;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class PortfolioController {
     
     // Serve JSP page
     @GetMapping({"/", "/portfolio"})
     public String home() {
         return "index"; // maps to /WEB-INF/jsp/index.jsp
-    }
-
-    // Serve JSON response
-    // GET API: /api/hello
-    @GetMapping("/api/hello")
-    public Map<String, String> helloApi() {
-        return Map.of("message", "Hi, I'm Jiocoders team");
-    }
-
-    // POST API: /api/hello
-    @PostMapping("/api/hello")
-    public Map<String, String> helloPost() {
-        return Map.of("message", "Hi, I'm Jiocoders team");
     }
 }
