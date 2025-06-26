463a4fd516d972feb82da0009f9b64002ed89da0
harry <hk.mca08@gmail.com>
2025-06-26 22:30:19 +0530

    update/xml.v5.9

--- src/main/java/com/jiocoders/portfolio/Jiocoders2206javaApplication.java
+++ src/main/java/com/jiocoders/portfolio/Jiocoders2206javaApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
 
 @SpringBootApplication
-public class Jiocoders2206javaApplication extends SpringBootServletInitializer {
+public class Jiocoders2206javaApplication extends SpringBootServletInitializer implements  ServletContextListener {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Jiocoders2206javaApplication.class, args);
@@ -20,6 +22,11 @@ public class Jiocoders2206javaApplication extends SpringBootServletInitializer {
         return builder.sources(Jiocoders2206javaApplication.class);
     }
 
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("🔥 ServletContext initialized. Spring Boot is starting...");
+    }
+	
 	@PostConstruct
 	public void init() {
 		System.out.println("PortfolioController initialized success");
