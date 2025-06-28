6c2af7e0a808ef6ac693f71ab07a1787157b7729
harry <hk.mca08@gmail.com>
2025-06-28 14:04:21 +0530

    update/Dockerfile.v9

--- src/main/java/com/jiocoders/portfolio/Jiocoders2206javaApplication.java
+++ src/main/java/com/jiocoders/portfolio/Jiocoders2206javaApplication.java
@@ -2,25 +2,29 @@ package com.jiocoders.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
 
 @SpringBootApplication
-public class Jiocoders2206javaApplication { // extends SpringBootServletInitializer implements  ServletContextListener {
+public class Jiocoders2206javaApplication extends SpringBootServletInitializer implements  ServletContextListener {
 	public static void main(String[] args) {
 		SpringApplication.run(Jiocoders2206javaApplication.class, args);
 		System.out.println("****** Application has been Started ******");
 	}
 	
-    // @Override
-    // protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-    //     return builder.sources(Jiocoders2206javaApplication.class);
-    // }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Jiocoders2206javaApplication.class);
+    }
 
-    // @Override
-    // public void contextInitialized(ServletContextEvent sce) {
-    //     System.out.println("🔥 ServletContext initialized. Spring Boot is starting...");
-    // }
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("🔥 ServletContext initialized. Spring Boot is starting...");
+    }
 	
 	@PostConstruct
 	public void init() {
