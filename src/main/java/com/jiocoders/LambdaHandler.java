5b9bb21f8ce9206c8a8f4e6ee1f88885c43631b4
harry <hk.mca08@gmail.com>
2025-07-05 07:53:16 +0530

    update/pom

---
+++ src/main/java/com/jiocoders/LambdaHandler.java
@@ -0,0 +1,7 @@
+package com.jiocoders;
+
+// import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+// public class LambdaHandler extends SpringBootRequestHandler<Object, Object> {
+
+// }
