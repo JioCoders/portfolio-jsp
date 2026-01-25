08ae4290dc671cecf7272987b35274cc59a7c964
Jiocoders <Jiocoders@gmail.com>
2026-01-25 23:38:26 +0530

    PORT-00|JIO|typo

--- src/main/java/com/jiocoders/portfolio/config/SecurityConfig.java
+++ src/main/java/com/jiocoders/portfolio/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)a
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/h2-console/**", "/", "portfolio", "/login", "/css/**", "/js/**",
                                 "/images/**")
