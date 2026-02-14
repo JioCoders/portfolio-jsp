29bd90dfe706d4937fc1014f55fbc0adba80cf7b
Jiocoders <Jiocoders@gmail.com>
2026-02-14 14:51:03 +0530

    feat: Introduce dedicated OpenAI chat endpoint, integrate Noupe AI Chat Bot, and update AI controller request mapping with `@NonNull` annotations.

--- src/main/java/com/jiocoders/portfolio/config/AIConfig.java
+++ src/main/java/com/jiocoders/portfolio/config/AIConfig.java
@@ -5,17 +5,18 @@ import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 
 @Configuration
 public class AIConfig {
 
 	@Bean(name = "openAiChatClient")
-	public ChatClient openAiChatModel(OpenAiChatModel chatModel) {
+	public ChatClient openAiChatModel(@NonNull OpenAiChatModel chatModel) {
 		return ChatClient.builder(chatModel).build();
 	}
 
 	@Bean(name = "ollamaChatClient")
-	public ChatClient ollamaChatModel(OllamaChatModel chatModel) {
+	public ChatClient ollamaChatModel(@NonNull OllamaChatModel chatModel) {
 		return ChatClient.builder(chatModel).build();
 	}
 
--- src/main/java/com/jiocoders/portfolio/controllers/AIChatController.java
+++ src/main/java/com/jiocoders/portfolio/controllers/AIChatController.java
@@ -1,39 +1,39 @@
 package com.jiocoders.portfolio.controllers;
 
 import org.springframework.ai.chat.client.ChatClient;
-// import org.springframework.ai.chat.model.ChatModel;
-// import org.springframework.ai.ollama.OllamaChatModel;
-// import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("${jio.api.prefix}/ai")
 public class AIChatController {
 
-	// private ChatClient openAiChatClient;
+	private ChatClient openAiChatClient;
 	private ChatClient ollamaChatClient;
 
 	public AIChatController(@Qualifier("openAiChatClient") ChatClient openAiChatClient,
 			@Qualifier("ollamaChatClient") ChatClient ollamaChatClient) {
-		// this.openAiChatClient = openAiChatClient;
+	System.out.println(openAiChatClient.getClass().getName());
+	System.out.println(ollamaChatClient.getClass().getName());
+		this.openAiChatClient = openAiChatClient;
 		this.ollamaChatClient = ollamaChatClient;
 	}
 
-	// public ChatController(OpenAiChatModel openAiChatModel, OllamaChatModel
-	// ollamaChatModel) {
-	/// / System.out.println(chatModel.getClass().getName());
-	/// / this.chatClient = ChatClient.builder(chatModel).build();
-	// this.openAiChatClient=ChatClient.builder(openAiChatModel).build();
-	// this.ollamaChatClient=ChatClient.builder(ollamaChatModel).build();
-
-	@GetMapping("/chat")
-	public ResponseEntity<String> chat(@RequestParam(value = "q", required = true) String q) {
+	@GetMapping("/chat-ollama")
+	public ResponseEntity<String> chatOllama(@RequestParam(value = "q", required = true) @NonNull String q) {
 		var resultResponse = this.ollamaChatClient.prompt(q).call().content();
+
+		return ResponseEntity.ok(resultResponse);
+	}
+
+	@GetMapping("/chat-openai")
+	public ResponseEntity<String> chatOpenAi(@RequestParam(value = "q", required = true) @NonNull String q) {
+		var resultResponse = this.openAiChatClient.prompt(q).call().content();
 
 		return ResponseEntity.ok(resultResponse);
 	}
